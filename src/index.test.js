cd164fdae19a2f4da735afb68135a84c55d3f400
Sofia Wiking <aswiking@gmail.com>
2022-01-07 15:18:13 +0000

    Add some more test values

--- src/index.test.js
+++ src/index.test.js
@@ -67,42 +67,28 @@ function makeTotal(currentSts, increaseSts) {
 }
 
 function totalIncreases(currentSts, increaseSts) {
-  const [
-    portions,
-    stdRep,
-    altRep,
-    standardSpacing,
-    firstPortion,
-    lastPortion,
-    stdRepRepeats,
-    altRepRepeats,
-  ] = shebang(currentSts, increaseSts);
-
-  console.log(
-    "stdRep",
-    stdRep,
-    "altRep",
-    altRep,
-    "standardSpacing",
-    standardSpacing,
-    "firstPortion",
-    firstPortion,
-    "lastPortion",
-    lastPortion
+  const [, , , , , , stdRepRepeats, altRepRepeats] = shebang(
+    currentSts,
+    increaseSts
   );
+
   return stdRepRepeats + altRepRepeats - 1;
-
-
 }
 
 test("generateReps returns correct values for portions, stdRep, altRep, altRepRepeats, stdRepRepeats", () => {
   expect(generateReps(100, 20)).toEqual([21, 4, 5, 16, 5]);
+  expect(generateReps(7, 4)).toEqual([5, 1, 2, 2, 3]);
 });
 
 test("Stitches from the variables that make the instructions string add up to 'current stitches' number", () => {
   expect(makeTotal(100, 20)).toEqual(100);
+  expect(makeTotal(7, 4)).toEqual(7);
+  expect(makeTotal(50, 4)).toEqual(50); //failing
 });
 
 test("Number of M1s add up to the increaseSts variable", () => {
   expect(totalIncreases(100, 20)).toEqual(20);
+  expect(totalIncreases(7, 4)).toEqual(4);
+  expect(totalIncreases(200, 4)).toEqual(4);
+  expect(totalIncreases(200, 30)).toEqual(30);
 });
